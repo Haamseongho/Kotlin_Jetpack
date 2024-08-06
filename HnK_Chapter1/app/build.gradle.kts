2b82d5dddf583e9ecaed40b508dd876b7889af2f
T005895 <haams704@gmail.com>
2024-08-06 17:51:48 +0900

    Feat: 우선 사진쪽은 해결+ 테스트 필요

--- HnK_Chapter1/app/build.gradle.kts
+++ HnK_Chapter1/app/build.gradle.kts
@@ -44,12 +44,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.annotations)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.compiler)
-
+    implementation(libs.androidx.room.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- HnK_Chapter1/app/src/main/java/com/example/diary/MainActivity.kt
+++ HnK_Chapter1/app/src/main/java/com/example/diary/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.diary
 
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -11,13 +13,21 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.diary.adapters.DiaryAdapter
 import com.example.diary.databinding.ActivityMainBinding
+import com.example.diary.models.DiaryContents
 import com.example.diary.models.REQUEST_IMAGE_PERMISSON
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var diaryAdapter: DiaryAdapter? = null
+    private val imageLauncher =
+        registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+            uploadImages(uriList)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,7 +41,6 @@ class MainActivity : AppCompatActivity() {
 
         initRecyclerView()
     }
-
     private fun initRecyclerView() {
         diaryAdapter = DiaryAdapter(object : DiaryAdapter.ItemClickListener {
             @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -39,12 +48,39 @@ class MainActivity : AppCompatActivity() {
                 checkImagePermission()
             }
         })
-        binding.mainRecyclerView
+        binding.mainRecyclerView.apply {
+            adapter = diaryAdapter
+            layoutManager = GridLayoutManager(context, 2)
+        }
     }
+
+    /*
+    1) registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList } -> 여러개 불러온 사진에 대한 uri 리스트
+    2) uriList.map을 통해서 내부의 uri를 불러올 수 있음
+    3) 모델이 되는 클래스 가지고와서 그 안에 uri 관리하는 data class에 접근해서 it(uri)를 넣어줌
+    4) 어댑터의 현재 리스트는 List로 되어 있기 때문에 값 추가를 할 수 없기 때문에 toMutableList()로 값 넣을 수 있게 한다.
+    5) apply 확장함수를 통해 MutableList의 addAll 함수를 통해 3번에서 처리한 이미지들을 다 넣어준다.
+    6) 어댑터의 submitList를 가져와서 추가한 이미지를 넣어준다.  Images, updatedImages 모두 리스트이다.  고로 submitList에 들어갈 수 있다.
+    7) submitList를 쓰게되면 ListAdapter쪽에서 데이터 관리를 알아서 해주고 추가되든 삭제되든 알아서 인덱스까지 관리해서 반환해준다.
+     */
+    private fun uploadImages(uriList: List<Uri>) {
+        // uriList = 선택한 이미지의 uri를 리스트로 관리
+        // uriList에 있는 image를 뽑아서 어댑터쪽에 이미지로 넣어주기
+        val images = uriList.map {
+            DiaryContents.DiaryItems(it) // 모델에서의 이미지의 Uri에 uriList에서 맵핑으로 가져오는 Uri를 넣어줌
+        }
+        // 내가 선택한 사진들 우선 아이템에 넣고 그 객체를 이제 리스트에 넣어줘야함
+        val updatedImages = diaryAdapter?.currentList?.toMutableList()?.apply {
+            addAll(images)
+        }
+
+        diaryAdapter?.submitList(updatedImages) // 이게 중요함 submitList에 넣어줘야 자동으로 데이터 관리하면서 리스트 셋팅함
+    }
+
 
     // 이미지 불러오기
     private fun loadImages() {
-
+        imageLauncher.launch("image/*")
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -63,8 +99,6 @@ class MainActivity : AppCompatActivity() {
                 requestPermissions()
             }
         }
-
-
     }
 
     // 질의할 것
@@ -77,7 +111,7 @@ class MainActivity : AppCompatActivity() {
                 requestPermissions()
             }
             setNegativeButton("거부", null)
-        }
+        }.show()
     }
 
     // 권한 줄 것
@@ -88,5 +122,22 @@ class MainActivity : AppCompatActivity() {
             arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
             REQUEST_IMAGE_PERMISSON
         )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_IMAGE_PERMISSON -> {
+                // grantResults에 결과가 들어가있는데 첫 번째꺼 접근
+                val resultCode = grantResults.firstOrNull() ?: PackageManager.PERMISSION_DENIED
+                if (resultCode == PackageManager.PERMISSION_GRANTED) {
+                    loadImages()
+                }
+            }
+        }
     }
 }
