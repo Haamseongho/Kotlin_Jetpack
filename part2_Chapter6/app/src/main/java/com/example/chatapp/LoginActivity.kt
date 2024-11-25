8f5f322b609b81e372d9087b55b0d8413aa71e5f
함성호 <hamseongho@hamseonghoui-MacBookAir.local>
2024-11-25 14:11:35 +0900

    feat: 채팅관련 내용 정리

--- part2_Chapter6/app/src/main/java/com/example/chatapp/LoginActivity.kt
+++ part2_Chapter6/app/src/main/java/com/example/chatapp/LoginActivity.kt
@@ -12,15 +12,15 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class LoginActivity : AppCompatActivity() {
+class   LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 로그인
-        binding.loginButton.setOnClickListener {
+        // 회원가입
+        binding.signUpButton.setOnClickListener {
             val email = binding.emailEditText.text.toString()
             val password = binding.passwordEditText.text.toString()
             if(email.length < 8 || email.isEmpty()) {
@@ -36,19 +36,19 @@ class LoginActivity : AppCompatActivity() {
                 .addOnCompleteListener(this){task ->
                     if(task.isSuccessful){
                         // 로그인 성공
-                        Toast.makeText(this, "로그인 성공!  로그인을 해주세요.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "회원가입 성공!  로그인을 해주세요.", Toast.LENGTH_SHORT).show()
                     } else {
                         // 회원가입 실패
-                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
                     }
                 }
         }
-        // 회원가입
+        // 로그인
         binding.signInButton.setOnClickListener {
             val email = binding.emailEditText.text.toString()
             val password = binding.passwordEditText.text.toString()
             if(email.length < 8 || email.isEmpty()) {
-                Toast.makeText(applicationContext, "이메일 형식을 지켜주세요.", Toast.LENGTH_SHORT).show()
+                 Toast.makeText(applicationContext, "이메일 형식을 지켜주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener // 클릭 리스너로 돌아가기
             }
 
@@ -67,13 +67,16 @@ class LoginActivity : AppCompatActivity() {
                         user["username"] = email
 
                         // 미국으로 안하게되면 database("url")
+                        // 예:) Firebase.database("https://~~~~~~~")
+                        // child에 Key로 해서 가져오는데 이건 많이 쓰이기 때문에 상수값으로 넣어둠
+                        // userId -> 내 아이디 (채팅방 관리할 때 쓰임) 알고 있기 때문에 child로 해서 다시 접근
                         Firebase.database.reference.child(DB_USERS).child(userId).updateChildren(user)
                         // 회원가입 성공
                         startActivity(Intent(this, MainActivity::class.java))
                         finish()
                     } else {
                         // 회원가입 실패
-                        Toast.makeText(this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
                         Log.d("LoginActivity", task.exception.toString())
                     }
                 }
--- part2_Chapter6/app/src/main/java/com/example/chatapp/userList/model/UserItem.kt
+++ part2_Chapter6/app/src/main/java/com/example/chatapp/userList/model/UserItem.kt
@@ -6,3 +6,23 @@ data class UserItem(
     val username: String ?= null,
     val description: String ?= null,
 )
+// Firebase
+// database.child("users").child(userId).setValue(user)
+/*
+영구 리스너로 데이터 읽기
+addValueEventListener() -> UserItem에 데이터가 변경할 때마다 계속 발생
+
+데이터 한 번만 읽기
+get()
+database.child("users").child(userId).get().addOnSuccessListener {
+
+}
+파이어베이스 -> addListenerForSingleValueEvent를 사용 -> 로컬 디스크 캐시에서 즉시 데이터 가져올 수 있음
+-> 사용자 프로필 로드할 때 사용
+
+
+* 데이터 목록 다루기
+1) 추가
+- push()
+- ChildEventListener
+ */
